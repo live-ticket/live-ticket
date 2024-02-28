60307c5d0f391ed17f14834c7fcb933bddf12c9a
Anwooseong <aws5624@naver.com>
2024-02-28 17:22:15 +0900

    init: dev 브랜치 생성

--- src/main/java/com/ll/ticket/LiveTicketApplication.java
+++ src/main/java/com/ll/ticket/LiveTicketApplication.java
@@ -2,7 +2,7 @@ package com.ll.ticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class LiveTicketApplication {
 
