745ec35ebf86fcfa9064678ac074c3a29a6145b7
gayoi <117894300+gayoi@users.noreply.github.com>
2024-02-28 16:51:25 +0900

    setup project

---
+++ src/main/java/com/ll/ticket/LiveTicketApplication.java
@@ -0,0 +1,13 @@
+package com.ll.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveTicketApplication.class, args);
+    }
+
+}
