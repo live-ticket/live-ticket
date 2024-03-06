5cd886a3b720a3982915a987e8438a83fb9de329
Aidennn-Lee <yw010804@gmail.com>
2024-03-06 17:52:51 +0900

    feat : 콘서트 등록 구현

--- src/main/java/com/ll/ticket/domain/admin/service/AdminService.java
+++ src/main/java/com/ll/ticket/domain/admin/service/AdminService.java
@@ -81,6 +81,7 @@ public class AdminService {
 
         this.concertRepository.save(concert);
     }
+<<<<<<< HEAD
 
     public void modify(RegisterConcertDto registerConcertDto, Concert concert, Place place, ConcertPerformer concertPerformer, List<ConcertDate> concertDates){
         String name = registerConcertDto.getName();
@@ -135,3 +136,6 @@ public class AdminService {
 
     }
 }
+=======
+}
+>>>>>>> 8b4294a (feat : 콘서트 등록 구현)
