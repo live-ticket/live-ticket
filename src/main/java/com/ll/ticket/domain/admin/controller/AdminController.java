6378847c2832c6eacf1a95e3273d99923d8bcd1d
Aidennn-Lee <yw010804@gmail.com>
2024-03-06 18:09:22 +0900

    fix : 병합 충돌 수정

--- src/main/java/com/ll/ticket/domain/admin/controller/AdminController.java
+++ src/main/java/com/ll/ticket/domain/admin/controller/AdminController.java
@@ -1,79 +1,80 @@
 package com.ll.ticket.domain.admin.controller;
 
 import com.ll.ticket.domain.admin.dto.RegisterConcertDto;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c7a7f29 (콘feat : 콘서트 등록 구현)
 import com.ll.ticket.domain.admin.service.AdminService;
-import com.ll.ticket.domain.place.entity.Place;
-import com.ll.ticket.global.enums.ConcertCategory;
-import com.ll.ticket.global.enums.ConcertStatus;
-<<<<<<< HEAD
-=======
->>>>>>> 2c0a23a (feat : 콘서트 등록 폼 작성)
-=======
->>>>>>> c7a7f29 (콘feat : 콘서트 등록 구현)
+import com.ll.ticket.domain.concert.entity.Concert;
+import com.ll.ticket.domain.concert.service.ConcertService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     private final AdminService adminService;
+    private final ConcertService concertService;
+
+    //관리자 페이지 메인화면
+    @GetMapping(value = "/main")
+    public String main(){
+        return "domain/admin/admin_main";
+    }
 
     //콘서트 등록
-    @GetMapping("/registerConcert")
+    @GetMapping(value = "/registerConcert")
     public String registerConcert(RegisterConcertDto registerConcertDto) {
-=======
-    //콘서트 등록
-    @GetMapping("/registerConcert")
-    public String registerConcert() {
->>>>>>> 2c0a23a (feat : 콘서트 등록 폼 작성)
-=======
-    private final AdminService adminService;
-
-    //콘서트 등록
-    @GetMapping("/registerConcert")
-    public String registerConcert(RegisterConcertDto registerConcertDto) {
->>>>>>> c7a7f29 (콘feat : 콘서트 등록 구현)
-        return "domain/concert/registerConcert";
+        return "domain/admin/register_concert";
     }
 
 
     //콘서트 등록
-    @PostMapping("/registerConcert")
+    @PostMapping(value = "/registerConcert")
     public String registerConcert(@Valid RegisterConcertDto registerConcertDto, BindingResult bindingResult) {
-<<<<<<< HEAD
-<<<<<<< HEAD
         if (bindingResult.hasErrors()) {
-            return "domain/concert/registerConcert";
+            return "domain/admin/register_concert";
         }
 
         adminService.register(registerConcertDto);
 
         return "redirect:/";
-=======
-        //TO DO 글 저장
-        return "domain/concert/registerConcert";
->>>>>>> 2c0a23a (feat : 콘서트 등록 폼 작성)
-=======
-        if (bindingResult.hasErrors()) {
-            return "domain/concert/registerConcert";
-        }
-        //TO DO 글 저장
-        adminService.register(registerConcertDto);
+    }
 
-        return "redirect:/";
->>>>>>> c7a7f29 (콘feat : 콘서트 등록 구현)
+    //콘서트 목록
+    @GetMapping(value = "/concertList")
+    public String concertList(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
+        Page<Concert> concertList = this.concertService.getConcertList(page);
+        model.addAttribute("concertList", concertList);
+
+        return "domain/admin/concert_list";
+    }
+
+    //관리자용 콘서트 글 상세 페이지
+    @GetMapping(value = "/concertDetail/{id}")
+    public String concertDetail(Model model, @PathVariable("id") Long id){
+        Concert concert = this.concertService.getConcert(id);
+        model.addAttribute("concert", concert);
+
+        return "domain/admin/concert_detail";
+    }
+
+    //콘서트 글 수정
+
+    //콘서트 글 삭제
+    //@PreAuthorize("hasRole('ADMIN')")
+    @GetMapping(value = "/concertDelete/{id}")
+    public String concertDelete(@PathVariable("id") Long id){
+        Concert concert = this.concertService.getConcert(id);
+        this.concertService.delete(concert);
+
+        return "redirect:/admin/concertList";
     }
 }
--- src/main/java/com/ll/ticket/domain/admin/dto/RegisterConcertDto.java
+++ src/main/java/com/ll/ticket/domain/admin/dto/RegisterConcertDto.java
@@ -1,15 +1,5 @@
 package com.ll.ticket.domain.admin.dto;
 
-<<<<<<< HEAD
-import com.ll.ticket.global.enums.ConcertCategory;
-import com.ll.ticket.global.enums.ConcertStatus;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-
-=======
 import com.ll.ticket.domain.place.entity.Place;
 import com.ll.ticket.global.enums.ConcertCategory;
 import com.ll.ticket.global.enums.ConcertStatus;
@@ -20,7 +10,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Time;
->>>>>>> 2c0a23a (feat : 콘서트 등록 폼 작성)
 import java.time.LocalDateTime;
 
 @Getter
@@ -29,37 +18,8 @@ public class RegisterConcertDto {
     @NotBlank(message = "제목을 입력하세요")
     private String name;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     @NotNull(message = "경도를 입력하세요")
     private Double longitude;
-
-    @NotNull(message = "위도를 입력하세요")
-    private Double latitude;
-
-    @NotNull(message = "예매 오픈 일시를 입력하세요")
-    private LocalDateTime releaseTime;
-
-    @NotNull(message = " 공연 시작 시간을 입력하세요")
-    private LocalDateTime startTime;
-
-    @NotNull(message = "공연 종료 시간을 입력하세요")
-    private LocalDateTime endTime;
-
-    private ConcertCategory category;
-
-    private ConcertStatus status;
-
-    @NotNull(message = "좌석 가격을 입력하세요")
-    @Min(0)
-    private Long seatPrice;
-=======
-    //@NotBlank(message = "장소를 입력하세요")
-    //private Place place;
-=======
-    @NotNull(message = "경도를 입력하세요")
-    private Double longitude;
->>>>>>> c7a7f29 (콘feat : 콘서트 등록 구현)
 
     @NotNull(message = "위도를 입력하세요")
     private Double latitude;
@@ -80,5 +40,4 @@ public class RegisterConcertDto {
     @NotNull(message = "좌석 가격을 입력하세요")
     @Min(0)
     private int seatPrice;
->>>>>>> 2c0a23a (feat : 콘서트 등록 폼 작성)
 }
