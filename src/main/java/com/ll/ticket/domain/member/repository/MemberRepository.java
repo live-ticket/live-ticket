2046aa27459111fcd131c1761cc371c3e706da8d
Anwooseong <aws5624@naver.com>
2024-03-07 16:53:25 +0900

    feat: 카카오 로그인 기능

--- src/main/java/com/ll/ticket/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ll/ticket/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.ll.ticket.domain.member.repository;
 
 import com.ll.ticket.domain.member.entity.Member;
+import com.ll.ticket.global.enums.LoginType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
+    boolean existsByEmailAndLoginType(String email, LoginType loginType);
     boolean existsByPhoneNumber(String phoneNumber);
 }
