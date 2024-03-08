f124fb186653f36b52eb2efb2fb2dbb9e8c2c61f
Anwooseong <aws5624@naver.com>
2024-03-08 15:12:59 +0900

    test: 로그인 실패, 로그아웃 테스트 코드

--- src/test/java/com/ll/ticket/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/ll/ticket/domain/member/controller/MemberControllerTest.java
@@ -1,8 +1,12 @@
 package com.ll.ticket.domain.member.controller;
 
 import com.ll.ticket.domain.member.dto.JoinRequest;
+import com.ll.ticket.domain.member.repository.MemberRepository;
 import com.ll.ticket.domain.member.service.MemberService;
 import com.ll.ticket.global.enums.Gender;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +19,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,24 +32,38 @@ class MemberControllerTest {
     MockMvc mockMvc;
     @Autowired
     MemberService memberService;
-//    @Autowired
-//    PasswordEncoder passwordEncoder;
+    @Autowired
+    MemberRepository memberRepository;
 
+    // 이 변수에 저장된 값은 beforeEach에서 생성한 회원의 email 값입니다.
+    private static final String TEST_MEMBER_EMAIL = "test1234@naver.com";
+    private static final String TEST_MEMBER_PASSWORD = "test1234@";
+
+    @BeforeEach
+    void beforeEach() {
+        JoinRequest joinRequest = new JoinRequest();
+        joinRequest.setName("스프링테스트");
+        joinRequest.setEmail(TEST_MEMBER_EMAIL);
+        joinRequest.setPassword(TEST_MEMBER_PASSWORD);
+        joinRequest.setPasswordConfirm(TEST_MEMBER_PASSWORD);
+        joinRequest.setBirthDay(LocalDate.now());
+        joinRequest.setPhoneNumber("010-8888-9999");
+        joinRequest.setGender(Gender.MALE);
+        memberService.join(joinRequest);
+    }
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.findByEmail(TEST_MEMBER_EMAIL)
+                .ifPresent(memberRepository::delete);
+    }
+
+    @DisplayName("로그인 성공")
     @Test
     void login() throws Exception {
-//        JoinRequest joinRequest = new JoinRequest();
-//        joinRequest.setName("스프링테스트");
-//        joinRequest.setEmail("test1234@naver.com");
-//        joinRequest.setPassword("test1234@");
-//        joinRequest.setPasswordConfirm("test1234@");
-//        joinRequest.setBirthDay(LocalDate.now());
-//        joinRequest.setPhoneNumber("010-2638-1927");
-//        joinRequest.setGender(Gender.MALE);
-//        memberService.join(joinRequest);
-
         mockMvc.perform(post("/members/login")
-                        .param("email", "test1234@naver.com")
-                        .param("password", "test1234@")
+                        .param("email", TEST_MEMBER_EMAIL)
+                        .param("password", TEST_MEMBER_PASSWORD)
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"))
@@ -52,4 +71,28 @@ class MemberControllerTest {
         ;
     }
 
+    @DisplayName("로그인 실패")
+    @Test
+    void login_fail() throws Exception {
+        mockMvc.perform(post("/members/login")
+                        .param("email", "11111111@naver.com")
+                        .param("password", TEST_MEMBER_PASSWORD)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/members/login?error"))
+                .andExpect(unauthenticated())
+        ;
+    }
+
+    @DisplayName("로그아웃")
+    @Test
+    void logout() throws Exception {
+        mockMvc.perform(post("/members/logout")
+                        .param("email", TEST_MEMBER_EMAIL)
+                        .param("password", TEST_MEMBER_PASSWORD)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+    }
 }
