ba97270525f66fac9a50ca20ecb733b703e99115
gayoi <117894300+gayoi@users.noreply.github.com>
2024-03-22 17:36:26 +0900

    fix :  아티스트 한글,영문명, 장소 검색수정

--- src/main/java/com/ll/ticket/domain/concert/repository/ConcertRepository.java
+++ src/main/java/com/ll/ticket/domain/concert/repository/ConcertRepository.java
@@ -1,7 +1,6 @@
 package com.ll.ticket.domain.concert.repository;
 
 import com.ll.ticket.domain.concert.entity.Concert;
-import com.ll.ticket.domain.place.entity.Place;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,15 +8,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-public interface ConcertRepository extends JpaRepository<Concert, Long> {
+public interface ConcertRepository extends JpaRepository<Concert, Long>, ConcertCustomRepository {
     Page<Concert> findAll(Pageable pageable);
 
-    @Query("SELECT c FROM Concert c join fetch c.place p " +
+    @Query("SELECT c FROM Concert c join fetch c.place p left join fetch c.concertPerformer cp " +
             "WHERE LOWER(c.concertNameKr) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(cp.artistNameEng) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
+            "OR LOWER(cp.artistNameKr) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
             "OR LOWER(c.name) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
             "OR LOWER(c.concertNameEng) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
-            "OR LOWER(p.placeName) LIKE LOWER(CONCAT('%', :keyword, '%'))"+
-            "OR LOWER(c.category) LIKE LOWER(CONCAT('%', :keyword, '%'))"+
+            "OR LOWER(p.placeName) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
+            "OR LOWER(c.category) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
             "AND c.status = 'ENABLE'")
     List<Concert> findByConcertKeyword(@Param("keyword") String keyword);
 }
