15c704b489774783efb1984ce47d736b580d0d20
gyeonghyeonkwon <83163894+gyeonghyeonkwon@users.noreply.github.com>
2024-03-27 02:20:36 +0900

    배포테스트 중단

--- src/test/java/com/ll/ticket/LiveTicketApplicationTests.java
+++ src/test/java/com/ll/ticket/LiveTicketApplicationTests.java
@@ -1,10 +1,13 @@
 package com.ll.ticket;
 
-//@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class LiveTicketApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
