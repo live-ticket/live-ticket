d031a914dd8ef3a2213bfcebe78987de2004673a
gyeonghyeonkwon <83163894+gyeonghyeonkwon@users.noreply.github.com>
2024-03-27 00:33:19 +0900

    테스트 파일 주석처리(원복예정)배포테스트2

--- src/test/java/com/ll/ticket/LiveTicketApplicationTests.java
+++ src/test/java/com/ll/ticket/LiveTicketApplicationTests.java
@@ -1,13 +1,10 @@
 package com.ll.ticket;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class LiveTicketApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
