fcab3981d01489c8dee245d2dcd8bf7c12ad3474
gayoi <117894300+gayoi@users.noreply.github.com>
2024-03-27 17:01:34 +0900

    feat:카테고리구현

--- src/main/java/com/ll/ticket/domain/concert/controller/ConcertController.java
+++ src/main/java/com/ll/ticket/domain/concert/controller/ConcertController.java
@@ -1,43 +1,52 @@
-//package com.ll.ticket.domain.concert.controller;
-//
-//import com.ll.ticket.domain.concert.entity.Concert;
-//import com.ll.ticket.domain.concert.entity.ConcertDate;
-//import com.ll.ticket.domain.concert.service.ConcertService;
-//import com.ll.ticket.global.security.config.SecurityUser;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import java.util.List;
-//
-//@Controller
-//@RequiredArgsConstructor
-//public class ConcertController {
-//    private final ConcertService concertService;
-//
-//    @GetMapping("/concert/{id}")
-//    public String getConcert(@PathVariable("id") Long id, Model model, @AuthenticationPrincipal SecurityUser user) {
-//        try {
-//            Concert concert = concertService.findById(id);
-//            List<ConcertDate> concertDate = concertService.findConcertDateByConcert(concert);
-//
-//            if (concertDate.isEmpty()) {
-//                throw new IllegalArgumentException("공연 날짜가 존재하지 않습니다.");
-//            }
-//
-//            model.addAttribute("concert", concert);
-//            model.addAttribute("concertDate", concertDate);
-//            model.addAttribute("member", user);
-//
-//            return "domain/concert/concert";
-//        } catch (IllegalArgumentException e) {
-//            model.addAttribute("errorMessage", e.getMessage());
-//            return "main";
-//        }
-//
-//
-//    }
-//}
+package com.ll.ticket.domain.concert.controller;
+
+import com.ll.ticket.domain.concert.entity.Concert;
+import com.ll.ticket.domain.concert.entity.ConcertDate;
+import com.ll.ticket.domain.concert.service.ConcertService;
+import com.ll.ticket.global.security.config.SecurityUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ConcertController {
+    private final ConcertService concertService;
+
+    @GetMapping("/concert/{id}")
+    public String getConcert(@PathVariable("id") Long id, Model model, @AuthenticationPrincipal SecurityUser user) {
+        try {
+            Concert concert = concertService.findById(id);
+            List<ConcertDate> concertDate = concertService.findConcertDateByConcert(concert);
+
+            if (concertDate.isEmpty()) {
+                throw new IllegalArgumentException("공연 날짜가 존재하지 않습니다.");
+            }
+
+            model.addAttribute("concert", concert);
+            model.addAttribute("concertDate", concertDate);
+            model.addAttribute("member", user);
+
+            return "domain/concert/concert";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "main";
+        }
+
+
+    }
+    @GetMapping("/concert/category/{type}")
+    public String getConcertByCategory(@PathVariable("type") String type, Model model, @AuthenticationPrincipal SecurityUser user) {
+        List<Concert> concerts = concertService.categoryConcertList(type);
+        System.out.println("concerts.size() = " + concerts.size());
+        model.addAttribute("concerts", concerts);
+        model.addAttribute("member", user);
+        return "domain/concert/category";
+    }
+
+}
--- src/main/java/com/ll/ticket/domain/concert/repository/ConcertRepository.java
+++ src/main/java/com/ll/ticket/domain/concert/repository/ConcertRepository.java
@@ -1,6 +1,7 @@
 package com.ll.ticket.domain.concert.repository;
 
 import com.ll.ticket.domain.concert.entity.Concert;
+import com.ll.ticket.global.enums.ConcertCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,4 +22,15 @@ public interface ConcertRepository extends JpaRepository<Concert, Long> {
             "OR LOWER(c.category) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
             "AND c.status = 'ENABLE'")
     List<Concert> findByConcertKeyword(@Param("keyword") String keyword);
+
+    @Query("SELECT distinct c FROM Concert c " +
+            "left join fetch c.place p " +
+            "left join fetch c.concertPerformer cp " +
+            "left join fetch c.images i " +
+            "join c.concertDates cd " +
+            "WHERE c.category = :type " +
+            "AND c.status = 'ENABLE'")
+    List<Concert> findByCategoryConcerts(@Param("type") ConcertCategory type);
+
+
 }
