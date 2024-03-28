2e3c41a190fb23c042ff3bba50a4ba79672443a9
Aidennn-Lee <yw010804@gmail.com>
2024-03-28 21:06:43 +0900

    feat: ë메인 화면 ì완료

--- src/main/java/com/ll/ticket/domain/admin/dto/RegisterConcertDto.java
+++ src/main/java/com/ll/ticket/domain/admin/dto/RegisterConcertDto.java
@@ -30,6 +30,9 @@ public class RegisterConcertDto {
     @NotBlank(message = "출연자 영문명을 입력하세요")
     private String artistNameEng;
 
+    @NotBlank(message = "장소명을 입력하세요")
+    private String placeName;
+
     @NotNull(message = "경도를 입력하세요")
     private Double longitude;
 
--- src/main/java/com/ll/ticket/domain/admin/service/AdminService.java
+++ src/main/java/com/ll/ticket/domain/admin/service/AdminService.java
@@ -94,6 +94,7 @@ public class AdminService {
         //Place 객체 빌드
         Place place = Place.builder()
                 .concert(concert)
+                .placeName(registerConcertDto.getPlaceName())
                 .longitude(registerConcertDto.getLongitude())
                 .latitude(registerConcertDto.getLatitude())
                 .totalPeople(registerConcertDto.getTotalPeople())
